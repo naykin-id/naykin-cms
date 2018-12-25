621181e04cb36fb4debe747a48275c1e9d8d2f5b
HOODYGOOD\RizqiReza <rizqireza.valhevi@gmail.com>
2018-12-25 19:34:32 +0700

    update yarn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAJM91XIOFDY89g38Mz9RSBdmTTpmDVk2U",
+    authDomain: "naykin-id.firebaseapp.com",
+    databaseURL: "https://naykin-id.firebaseio.com",
+    projectId: "naykin-id",
+    storageBucket: "naykin-id.appspot.com",
+    messagingSenderId: "928070402383"
+  }
 };
